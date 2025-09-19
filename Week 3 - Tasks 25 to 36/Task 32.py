13b1d9021bcf9dc31649eaafb1502f213e804128
George_Cooke <georgecooke8809@gmail.com>
2025-09-19 19:00:30 +0100

    Added custom sorting algorithm to task 32

--- Week 3 - Tasks 25 to 36/Task 32.py
+++ Week 3 - Tasks 25 to 36/Task 32.py
@@ -1,14 +1,34 @@
-def sort(names):
-    pass
-    #TODO: Finish this function
+def sort(in_list):
+    ordered_list = in_list
+
+    length = len(in_list)
+
+    for i in range(length-1):
+        current = in_list[i+1]
+        sorted = False
+        index = 0
+
+        while sorted == False:
+            if in_list[index] > current:
+                ordered_list.pop(i+1)
+                ordered_list.insert(index, current)
+                sorted = True
+            elif in_list[index] == current:
+                ordered_list.pop(i+1)
+                ordered_list.insert(index, current)
+                sorted = True
+            elif in_list[index] < current:
+                index += 1
+
+            return ordered_list
 
 names = []
 
-for i in range(3):
+for i in range(3): # TODO: Make this continue until the user says stop
     name = input(f"Name {i+1}: ")
     names.append(name)
 
-ordered_names = sort(names)
+names = sort(names)
 
-for i in range(3):
-    print(f"Ordered Name {i+1}: {ordered_names[i]}")
+for i in range(3): # TODO: Make this print on one line
+    print(f"Ordered Name {i+1}: {names[i]}")
