132ecb91efcf4156b05a8aca76b4a22e0c8ba1de
George_Cooke <georgecooke8809@gmail.com>
2025-09-19 09:33:48 +0100

    Finished Task 31

--- Week 3 - Tasks 25 to 36/Task 31.py
+++ Week 3 - Tasks 25 to 36/Task 31.py
@@ -1,7 +1,12 @@
 age = int(input("Age: "))
 licence = input("Do You Have A Drivers Licence (Y/N): ")
 
-if licence.upper == "Y":
+if licence.upper() == "Y":
     licence = True
-elif licence.upper == "N":
+elif licence.upper() == "N":
     licence = False
+
+if age > 21 and licence == True:
+    print("You can drive the mini bus.")
+else:
+    print("You cannot drive the mini bus.")
