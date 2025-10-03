43c1d2813c0f1fd01484c3d6c32354f817d65032
George_Cooke <georgecooke8809@gmail.com>
2025-10-03 14:31:38 +0100

    Spelling Task 37

--- Extensions/Task 37.py
+++ Extensions/Task 37.py
@@ -1,15 +1,15 @@
-ammount = int(input("Go Up To: "))
+amount = int(input("Go Up To: "))
 
 string = ""
 
-for i in range(ammount):
+for i in range(amount):
     string = f"{string:<4}{i+1:<4}"
 
 print(string)
 
-for i in range(ammount):
+for i in range(amount):
     string = f"{i+1:<4}"
-    for j in range(ammount):
+    for j in range(amount):
         string = f"{string}{(i+1)*(j+1):<4}"
 
     print(string)
